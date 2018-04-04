aab152b391c28ab94995db5e09c7a1d25dd75d8b
nandishs89 <35736116+nandishs89@users.noreply.github.com>
2018-04-04 10:26:50 +0530

    Commit For the Fist Time
    Creating the Project

---
+++ SelfCarePortal/SelfCarePortal/SelfCarePortal_UI/Home.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SelfCarePortal.SelfCarePortal_UI.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+<h1> Hello World !</h1>
+</asp:Content>
---
+++ SelfCarePortal/SelfCarePortal_Models/ViewModels/EmployeeBasicInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCarePortal_Models.ViewModels
+{
+    public class EmployeeBasicInfo
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+        public string Gender { get; set; }
+        public string Designation { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+
+}
